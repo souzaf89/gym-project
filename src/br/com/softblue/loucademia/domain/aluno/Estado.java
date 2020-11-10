e7a39ada0f5fe7b9f2356f9e4e3686a6139f0465
Fagner Souza <fagner.roberto89@gmail.com>
2020-11-10 07:32:37 -0300

    Mapping the first entity in JPA

--- src/br/com/softblue/loucademia/domain/aluno/Estado.java
+++ src/br/com/softblue/loucademia/domain/aluno/Estado.java
@@ -1,9 +1,22 @@
 package br.com.softblue.loucademia.domain.aluno;
 
-public class Estado {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ESTADO")
+public class Estado implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name= "SIGLA", nullable = false, length = 2)
 	private String sigla;
 	
+	@Column (name = "NOME", nullable = false, length = 30)
 	private String nome;
 
 	public String getSigla() {
