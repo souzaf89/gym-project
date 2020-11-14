0b6fdf3ba8361ba337bc6b8d6072cd0c5feebfb9
Fagner Souza <fagner.roberto89@gmail.com>
2020-11-14 14:03:10 -0300

    Loading the states registered in the database

--- src/br/com/softblue/loucademia/interfaces/shared/web/DataBean.java
+++ src/br/com/softblue/loucademia/interfaces/shared/web/DataBean.java
@@ -1,11 +1,13 @@
-package br.com.softblue.loucademia.interfaces.web;
+package br.com.softblue.loucademia.interfaces.shared.web;
 
 import java.io.Serializable;
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
+import br.com.softblue.loucademia.application.service.DataService;
 import br.com.softblue.loucademia.domain.aluno.Aluno.Sexo;
 import br.com.softblue.loucademia.domain.aluno.Aluno.Situacao;
 import br.com.softblue.loucademia.domain.aluno.Estado;
@@ -15,15 +17,18 @@ import br.com.softblue.loucademia.domain.aluno.Estado;
 public class DataBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+		@EJB
+		private DataService dataService;
+		
 		public Sexo[] getSexos() {
-			return Sexo.values();
+			return dataService.getSexos();
 		}
 		
 		public Situacao[] getSituacoes() {
-			return Situacao.values();
+			return dataService.getSituacoes();
 		}
 		
 		public List<Estado> getEstados() {
-			return null;
+			return dataService.listEstados();
 		}
 }
