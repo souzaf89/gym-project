ad4a30764e6b71db38b66feac135676927f1d05c
Fagner Souza <fagner.roberto89@gmail.com>
2020-11-22 15:19:49 -0300

    Accessing the input and output report from the search

--- src/br/com/softblue/loucademia/interfaces/acesso/web/RelatorioEntradaSaidaBean.java
+++ src/br/com/softblue/loucademia/interfaces/acesso/web/RelatorioEntradaSaidaBean.java
@@ -12,6 +12,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.softblue.loucademia.application.service.AlunoService;
+import br.com.softblue.loucademia.application.util.StringUtils;
 import br.com.softblue.loucademia.application.util.ValidationException;
 import br.com.softblue.loucademia.domain.acesso.Acesso;
 
@@ -31,6 +32,12 @@ public class RelatorioEntradaSaidaBean implements Serializable{
 	private LocalDate dataFinal;
 	
 	private List<Acesso> acessos;
+	
+	public void carregarAluno() {
+		if (!StringUtils.isEmpty(matricula)) {
+			gerarRelatorio();
+		}
+	}
 	
 	public String gerarRelatorio() {
 		try {
